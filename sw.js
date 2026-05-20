e45c15be09746df55d7f4e2f9bdcae6bb074f15a
eggozer <fabian10109@gmail.com>
2026-05-20 15:04:02 -0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
+// Service Worker básico para permitir la instalación de Retórica
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting());
+});
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  // Permite cargar los archivos normalmente de la red
+  event.respondWith(fetch(event.request));
+});
