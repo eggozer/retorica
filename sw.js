7956c8aa33cd9bfa8b12d92011bfc468acbb21da
eggozer <fabian10109@gmail.com>
2026-05-22 17:59:43 -0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,43 @@
-const CACHE_NAME = 'retorica-cache-v17.9';
+var CACHE_NAME = "retorica-cache-v23";
+var urlsToCache = [
+  "/retorica/",
+  "/retorica/index.html",
+  "/retorica/manifest.json"
+];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll([
-                '/retorica/index.html',
-                '/retorica/manifest.json'
-            ]);
-        }).then(() => self.skipWaiting())
-    );
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cache) {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        }).then(() => self.clients.claim())
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Escucha el mensaje del botón para actualizar la app al instante
+self.addEventListener("message", function(event) {
+  if (event.data && event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
 });
